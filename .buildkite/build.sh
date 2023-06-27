9f473d1f4242cecde98ff3aab88ee2f8a0f2b142
k-Richie <126113214+k-Richie@users.noreply.github.com>
2023-06-27 12:05:33 +0530

    Update build.sh

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -4,8 +4,13 @@ set -eu
 
 echo "steps:"
 
-echo "  - command: \"docker build -t my-nginx-image -f docker/Dockerfile ./docker\""
-echo "    label: \"Build Nginx\""
+# Add dynamic steps based on Dockerfiles in the "docker" directory
+find docker -name Dockerfile | while read -r DOCKERFILE; do
+  IMAGE_NAME=$(basename "$(dirname "$DOCKERFILE")")
+  echo "  - command: \"docker build -t my-image:$IMAGE_NAME -f $DOCKERFILE .\""
+  echo "    label: \"$IMAGE_NAME\""
+done
+
 
 
 
