e1d709ab6322a775e1a7bfbf1b790f7dd3b019d4
k-Richie <126113214+k-Richie@users.noreply.github.com>
2023-06-27 12:51:47 +0530

    Update build.sh

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-echo "steps:"
-echo "  - label: \":docker: Build Docker Image\""
-echo "    command:"
-echo "      - docker build -t my-docker-image:latest docker/"
+# Copy the Dockerfile and nginx.conf to the build context
+cp docker/Dockerfile .
+cp docker/nginx.conf .
+
+# Build the Docker image
+docker build -t my-docker-image:latest .
+
+# Save the Docker image as an artifact
+docker save my-docker-image:latest -o docker/image.tar.gz
+
 
 
 
