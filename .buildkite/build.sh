c6343d5c9ca740728490506e4bfd120d17ee63e2
k-Richie <126113214+k-Richie@users.noreply.github.com>
2023-06-27 11:52:54 +0530

    Update build.sh

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -7,10 +7,6 @@ echo "steps:"
 echo "  - command: \"docker build -t my-nginx-image -f docker/Dockerfile ./docker\""
 echo "    label: \"Build Nginx\""
 
-if [[ "$BUILDKITE_BRANCH" == "main" ]]; then
-  echo "  - wait"
-  echo "  - command: \"echo Deploy!\""
-  echo "    label: \":rocket:\""
-fi
+
 
 
